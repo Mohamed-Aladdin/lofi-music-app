4752b3b7dabb1bb10cbcb6ce5c0d87bdb6c6040d
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-10-03 18:30:55 +0000

    Add uuid module and controllers boilerplate

---
+++ controllers/AuthController.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+import { v4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+export default class AuthController {
+    static async login(req, res, next) {
+        try {
+            
+        } catch (err) {
+            
+        }
+    }
+}
---
+++ controllers/UsersController.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+    static async getUser(req, res, next) {
+        try {
+            
+        } catch (err) {
+            
+        }
+    }
+}
